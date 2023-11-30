43b78eaa04c15451660c3733c17b192740720723
fortunam-MACID <144068694+fortunam-MACID@users.noreply.github.com>
2023-11-30 17:49:27 -0500

    Add files via upload

--- Project.cpp
+++ Project.cpp
@@ -3,15 +3,15 @@
 #include "objPos.h"
 #include "GameMechs.h"
 #include "Player.h"
+#include "objPosArrayList.h"
 
 using namespace std;
 
 #define DELAY_CONST 100000
 
 GameMechs* game;
-Player* player;
-objPos food;
-objPos playerPos;
+Player* player; 
+
 
 void Initialize(void);
 void GetInput(void);
@@ -22,12 +22,14 @@ void CleanUp(void);
 
 int main(void){
     Initialize();
-    while(game->getExitFlagStatus() == false){
+    while(game->getExitFlagStatus() == false)  
+    {
         GetInput();
         RunLogic();
         DrawScreen();
         LoopDelay();
     }
+
     CleanUp();
 }
 
@@ -37,8 +39,7 @@ void Initialize(void){
 
     game = new GameMechs(26,13);
     player = new Player(game);
-    game->generateFood(playerPos);
-    game->getFoodPos(food);
+
 }
 
 void GetInput(void){
@@ -47,40 +48,75 @@ void GetInput(void){
 
 void RunLogic(void){
     player->movePlayer();
-    game->clearInput();
 }
 
 void DrawScreen(void){
     MacUILib_clearScreen();
+    int p;
+    bool draw;
+    objPosArrayList *playerbody;
+    playerbody=player->getPlayerPos(); 
+    objPos tempbody;
+    objPos playerPos;
     
-    player->getPlayerPos(playerPos);
-    for (int i = 0; i < game->getBoardSizeY(); i++){
-        for (int j = 0; j < game->getBoardSizeX(); j++){
-            if(j == game->getBoardSizeX()-1){
-                MacUILib_printf("#\n");
+   
+    
+        for (int i = 0; i < game->getBoardSizeY(); i++){
+            for (int j = 0; j < game->getBoardSizeX(); j++)
+            {
+                draw=false;
+
+                for(int k=0; k< playerbody->getSize(); k++ )
+                {
+                    playerbody->getElement(tempbody,k);
+                    if(tempbody.x==j && tempbody.y==i )
+                    {
+                        MacUILib_printf("%c", tempbody.symbol);
+                        draw=true; 
+                        break;
+                    }
+                }
+                if(draw)
+                {
+                    continue;
+                }
+                if(j == game->getBoardSizeX()-1){
+                    MacUILib_printf("#\n");
+                }
+                else if (i == 0 || j == 0 || i == game->getBoardSizeY()-1){
+                    MacUILib_printf("#");
+                }      
+                
+                
+                
+                else{
+                    /*
+                    hasChar = 0;
+                    for(k = 0; k < 5; k++){
+                        if((i == itemBin[k].y) && (j == itemBin[k].x)){
+                            MacUILib_printf("%c", itemBin[k].symbol);
+                            hasChar = 1;
+                        }
+                    }*/
+                    //if (hasChar == 0) 
+                    MacUILib_printf(" ");
+                    
+                }
+               
             }
-            else if (i == 0 || j == 0 || i == game->getBoardSizeY()-1){
-                MacUILib_printf("#");
-            }      
-            else if(i == playerPos.y && j == playerPos.x){
-                MacUILib_printf("%c", playerPos.symbol);
-            }
-            else{
-                int spaceCondition = 0;
-                if((i == food.y) && (j == food.x)){
-                    MacUILib_printf("%c", food.symbol);
-                    spaceCondition = 1;
-                } 
-                if (spaceCondition == 0){
-                    MacUILib_printf(" ");   
-                } 
-            }
+        // [TODO]   Display the "Mystery String" contents at the bottom of the game board
+        //          To help players keep track of their game progress.
         }
-    }
+        //for (i = 0; i < my_strlen(goalString); i++) MacUILib_printf("%c", mysteryString[i]);
+        //MacUILib_printf("\nMove Count: %d", moveCount);
+        //MacUILib_printf("\nPress space to exit at anytime\n");
+        
+
 }
 
 void LoopDelay(void){
-    MacUILib_Delay(DELAY_CONST); // 0.1s delay
+    
+    MacUILib_Delay(200000); // 0.1s delay
 }
 
 void CleanUp(void){
